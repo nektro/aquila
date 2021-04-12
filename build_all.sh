06b4aebccc9d333e1baba48378c4fffcbdcc95df
Meghan Denny <hello@nektro.net>
2021-04-12 01:17:15 -0700

    add build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+build_template() {
+    export CGO_ENABLED=1
+    export GOOS=$1
+    export GOARCH=$2
+    export GOARM=7
+    ext=$3
+    date=$(date +'%Y%m%d')
+    version=${CIRCLE_BUILD_NUM-$date}
+    tag=v$version
+    echo $tag-$GOOS-$GOARCH
+    go build -ldflags="-s -w -X main.Version=$tag" -o ./bin/mantle-$tag-$GOOS-$GOARCH$ext
+}
+
+go get -v github.com/rakyll/statik
+$GOPATH/bin/statik -src="./www/" -f
+
+build_template linux amd64
