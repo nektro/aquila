6fd256aabad3c37aae32ec511635cbbfbd496475
Meghan Denny <hello@nektro.net>
2021-04-12 01:25:38 -0700

    Create make_release.sh

---
+++ make_release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+date=$(date +'%Y.%m.%d')
+version=${CIRCLE_BUILD_NUM-$date}
+tag=v$version
+go get github.com/tcnksm/ghr
+$GOPATH/bin/ghr \
+    -t ${GITHUB_TOKEN} \
+    -u ${CIRCLE_PROJECT_USERNAME} \
+    -r ${CIRCLE_PROJECT_REPONAME} \
+    -b "$(./changelog.sh)" \
+    "$tag" \
+    "./bin/"
