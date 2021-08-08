2a400d9c6b534c537e387e12adc7ac2195da16cc
Meghan Denny <hello@nektro.net>
2021-08-07 22:51:57 -0700

    add missing src/strings.zig

---
+++ src/strings.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+const string = []const u8;
+
+pub fn splitAlloc(alloc: *std.mem.Allocator, input: string, delim: string) ![]const string {
+    const result = &std.ArrayList(string).init(alloc);
+    defer result.deinit();
+    var it = std.mem.split(input, delim);
+    while (it.next()) |item| {
+        try result.append(item);
+    }
+    return result.toOwnedSlice();
+}
+
+pub const list = struct {
+    //
+
+    pub fn contains(haystack: []const string, needle: string) bool {
+        for (haystack) |item| {
+            if (std.mem.eql(u8, item, needle)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
