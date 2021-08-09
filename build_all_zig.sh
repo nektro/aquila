66bd794f43ae362cb2a6ed3a29ce6149d7dfe4ee
Meghan Denny <hello@nektro.net>
2021-08-09 02:36:17 -0700

    zig- include binaries in release

--- build_all_zig.sh
+++ build_all_zig.sh
@@ -15,6 +15,6 @@ x86_64-linux-musl
 for item in $targets
 do
     echo "$version.$rev $item"
-    zig build -Dtarget=$item -Duse-full-name -Dversion=$version.$rev
+    zig build -Dtarget=$item -Duse-full-name -Dversion=$version.$rev --prefix .
     echo
 done
