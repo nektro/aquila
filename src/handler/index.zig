050ec982dc2b591b63e819b4fa87c73fbbafd70a
Meghan Denny <hello@nektro.net>
2021-08-14 19:00:03 -0700

    zig- www/index- change `user:?` to `logged_in:false` since logging in isnt added yet

--- src/handler/index.zig
+++ src/handler/index.zig
@@ -4,9 +4,8 @@ const http = @import("apple_pie");
 const _internal = @import("./_internal.zig");
 
 pub fn get(_: void, response: *http.Response, request: http.Request) !void {
-    const user: ?usize = null;
     try _internal.writePageResponse(response, request, "/index.pek", .{
         .aquila_version = @import("root").version,
-        .user = user,
+        .logged_in = false,
     });
 }
