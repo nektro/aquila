ce7f0c44175ded9437a1d330773ae0ebc79b2bbe
Meghan Denny <hello@nektro.net>
2021-08-14 19:01:08 -0700

    zig- update pek and it accepts an Allocator now

--- src/handler/_internal.zig
+++ src/handler/_internal.zig
@@ -4,7 +4,7 @@ const http = @import("apple_pie");
 const files = @import("self/files");
 const pek = @import("pek");
 
-pub fn writePageResponse(response: *http.Response, request: http.Request, comptime name: []const u8, data: anytype) !void {
+pub fn writePageResponse(alloc: *std.mem.Allocator, response: *http.Response, request: http.Request, comptime name: []const u8, data: anytype) !void {
     _ = request;
     try response.headers.put("Content-Type", "text/html");
 
@@ -12,5 +12,5 @@ pub fn writePageResponse(response: *http.Response, request: http.Request, compti
     const head = comptime files.open("/_header.pek").?;
     const page = comptime files.open(name) orelse @compileError("file '" ++ name ++ "' not found in your files cache");
     const tmpl = comptime pek.parse(head ++ page);
-    try pek.compile(w, tmpl, data);
+    try pek.compile(alloc, w, tmpl, data);
 }
--- src/handler/index.zig
+++ src/handler/index.zig
@@ -4,7 +4,7 @@ const http = @import("apple_pie");
 const _internal = @import("./_internal.zig");
 
 pub fn get(_: void, response: *http.Response, request: http.Request) !void {
-    try _internal.writePageResponse(response, request, "/index.pek", .{
+    try _internal.writePageResponse(request.arena, response, request, "/index.pek", .{
         .aquila_version = @import("root").version,
         .logged_in = false,
     });
