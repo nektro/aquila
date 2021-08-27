b735c32c1a06be7532d500231ef10faf988f65db
Meghan Denny <hello@nektro.net>
2021-08-27 01:13:17 -0700

    move addSentinel into zig-extras

--- src/db/_.zig
+++ src/db/_.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const zorm = @import("zorm");
+const extras = @import("extras");
 
 const _internal = @import("./_internal.zig");
 const db = &_internal.db;
@@ -7,20 +8,12 @@ const Engine = _internal.Engine;
 
 pub fn connect(alloc: *std.mem.Allocator, path: []const u8) !void {
     const abspath = try std.fs.realpathAlloc(alloc, path);
-    const nulpath = try addSentinel(alloc, u8, abspath, 0);
+    const nulpath = try extras.addSentinel(alloc, u8, abspath, 0);
     db.* = try Engine.connect(nulpath);
 }
 
 pub fn close() void {
     db.close();
-}
-
-fn addSentinel(alloc: *std.mem.Allocator, comptime T: type, input: []const T, comptime sentinel: T) ![:sentinel]const T {
-    var list = try std.ArrayList(T).initCapacity(alloc, input.len + 1);
-    try list.appendSlice(input);
-    try list.append(sentinel);
-    const str = list.toOwnedSlice();
-    return str[0 .. str.len - 1 :sentinel];
 }
 
 pub const Remote = @import("./Remote.zig").Remote;
