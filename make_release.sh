d96341b692b968e77b817f1794ae894e4d0f75b7
Meghan Denny <hello@nektro.net>
2021-09-11 01:36:00 -0700

    make_release.sh- add space between ghr install and output

--- make_release.sh
+++ make_release.sh
@@ -5,7 +5,10 @@ set -e
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
 tag=v$version
+
 zigmod aq install 1/nektro/ghr
+echo
+
 ~/.zigmod/bin/ghr \
     -t ${GITHUB_TOKEN} \
     -u ${CIRCLE_PROJECT_USERNAME} \
