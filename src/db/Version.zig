b80b864b99af1571f0f1a0e79a968126df2df482
Meghan Denny <hello@nektro.net>
2021-12-13 01:31:01 -0800

    zig/db- add Version.setVersion

--- src/db/Version.zig
+++ src/db/Version.zig
@@ -19,7 +19,7 @@ pub const Version = struct {
     files: string,
     tar_size: u64,
     tar_hash: string,
-    approved_by: ulid.ULID,
+    approved_by: string, // TODO remove this column, app design changed
     real_major: u32,
     real_minor: u32,
     deps: string,
@@ -37,5 +37,11 @@ pub const Version = struct {
         _ = fmt;
         _ = options;
         try writer.print("v{d}.{d}", .{ self.real_major, self.real_minor });
+    }
+
+    pub fn setVersion(self: Version, alloc: *std.mem.Allocator, approver: User, major: u32, minor: u32) !void {
+        try self.updateColumn(alloc, .approved_by, try approver.uuid.toString(alloc));
+        try self.updateColumn(alloc, .real_major, major);
+        try self.updateColumn(alloc, .real_minor, minor);
     }
 };
