eb5fa5eb1bc19f829f9cbc47f77445117bbeb6f8
Meghan Denny <hello@nektro.net>
2022-01-06 04:49:40 -0800

    add build_release.sh

---
+++ build_release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+version="r$(./release_num.sh)"
+rev=$(git log --format=%h -1)
+
+target=$1
+
+# TODO error if $target is empty
+
+echo "$version.$rev $target"
+zig build -Dtarget=$target -Duse-full-name -Dversion=$version.$rev --prefix . -Drelease
