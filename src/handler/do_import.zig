832ab8d49eb879062cd9dfe66d9e0edf9d8fd333
Meghan Denny <hello@nektro.net>
2022-01-21 12:33:53 -0800

    zig- zigmod.ci now requires an allocator

--- src/handler/do_import.zig
+++ src/handler/do_import.zig
@@ -59,7 +59,7 @@ pub fn get(_: void, response: *http.Response, request: http.Request, args: struc
 
     try _internal.assert(try extras.doesFileExist(dir, "zigmod.lock"), response, "error: repository '{s}' does not contain a zigmod.lock file.", .{repo});
     const cachepath = try std.fs.path.join(alloc, &.{ ".zigmod", "deps" });
-    zigmod.commands.ci.do(cachepath, dir) catch |err| return _internal.fail(response, "error: zigmod ci failed: {s}\n", .{@errorName(err)});
+    zigmod.commands.ci.do(alloc, cachepath, dir) catch |err| return _internal.fail(response, "error: zigmod ci failed: {s}", .{@errorName(err)});
     try dir.deleteFile("deps.zig");
     const totalsize = try _internal.dirSize(alloc, path);
     try dir.deleteTree(".zigmod");
