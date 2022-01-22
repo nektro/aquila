bc6a5a87e1400f73287b370d633ffb322608f961
Meghan Denny <hello@nektro.net>
2022-01-22 00:46:10 -0800

    zig/handler/package- add stub for `zigmod aq add` support to work

--- src/handler/package.zig
+++ src/handler/package.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const string = []const u8;
 const http = @import("apple_pie");
 
@@ -11,6 +12,19 @@ pub fn get(_: void, response: *http.Response, request: http.Request, args: struc
     const p = try _internal.reqPackage(request, response, o, args.package);
     const v = try p.versions(alloc);
 
+    const h = try request.headers(alloc);
+    if (std.mem.eql(u8, h.get("accept") orelse "", "application/json")) {
+        // stub for `zigmod aq add x/y/z`
+        // TODO fill out json api
+        try response.headers.put("Content-Type", "application/json");
+        try std.json.stringify(
+            JsonStub{ .repo = .{ .domain = r.domain }, .pkg = .{ .RemoteName = p.remote_name, .remote_name = p.remote_name } },
+            .{},
+            response.writer(),
+        );
+        return;
+    }
+
     try _internal.writePageResponse(alloc, response, request, "/package.pek", .{
         .aquila_version = @import("root").version,
         .user = u,
@@ -20,3 +34,9 @@ pub fn get(_: void, response: *http.Response, request: http.Request, args: struc
         .versions = v,
     });
 }
+
+// Fill in since stage1 messes up on nested anonymous structs
+const JsonStub = struct {
+    repo: struct { domain: string },
+    pkg: struct { RemoteName: string, remote_name: string },
+};
