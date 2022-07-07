699d718c7c8e755b16e43f763e37cd9a0d748f05
Meghan Denny <hello@nektro.net>
2022-07-06 17:52:57 -0700

    build_release.sh: add --prominent-compile-errors

--- build_release.sh
+++ build_release.sh
@@ -10,4 +10,4 @@ target=$1
 # TODO error if $target is empty
 
 echo "$version.$rev $target"
-$(which time) zig build -Dtarget=$target -Duse-full-name -Dversion=$version --prefix . -Drelease
+$(which time) zig build -Dtarget=$target -Duse-full-name -Dversion=$version --prefix . -Drelease --prominent-compile-errors
