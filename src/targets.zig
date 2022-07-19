a2ba1ccc4cf73221fb3e13d1f232a4e2ab23047e
Meghan Denny <hello@nektro.net>
2022-07-19 13:07:20 -0700

    src/targets.zig: remove 32bit x86 from list

--- src/targets.zig
+++ src/targets.zig
@@ -3,20 +3,16 @@ const Arch = std.Target.Cpu.Arch;
 
 pub const debian = std.ComptimeStringMap(?Arch, .{
     .{ "amd64", .x86_64 },
-    .{ "i386", .i386 },
 });
 
 pub const alpine = std.ComptimeStringMap(?Arch, .{
     .{ "x86_64", .x86_64 },
-    .{ "x86", .i386 },
 });
 
 pub const freebsd = std.ComptimeStringMap(?Arch, .{
     .{ "amd64", .x86_64 },
-    .{ "i386", .i386 },
 });
 
 pub const netbsd = std.ComptimeStringMap(?Arch, .{
     .{ "amd64", .x86_64 },
-    .{ "i386", .i386 },
 });
