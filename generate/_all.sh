ba9d1762156f1e556a211a40c75642909ffe880e
Meghan Denny <hello@nektro.net>
2022-07-27 12:18:48 -0700

    generate/_all.sh: run all stage2 instead of all arches for an os

--- generate/_all.sh
+++ generate/_all.sh
@@ -6,9 +6,9 @@ set -x
 # Meta script for building all architectures of an OS
 
 os="$1"
+arch="$2"
 
-# cc https://github.com/ziglang/zig/issues/10754
-for arc in $(zig run tools/os-list-arches.zig --main-pkg-path . -- "$os")
-do
-    ./generate/$os.sh $arc
-done
+./generate/$os.sh $arch
+./tools/gen_stage2.sh $os $arch
+./tools/gen_stage3.sh $os $arch
+./tools/gen_stage4.sh $os $arch
